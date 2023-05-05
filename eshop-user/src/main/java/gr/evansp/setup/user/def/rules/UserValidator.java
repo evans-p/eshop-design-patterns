0bbf5373cd2246b744e2039a7faf642ae8e6aba6
evans-p <evanspoulaki@gmail.com>
2023-05-06 00:58:32 +0300

    Added validation interface for User.

---
+++ eshop-user/src/main/java/gr/evansp/setup/user/def/rules/UserValidator.java
@@ -0,0 +1,19 @@
+package gr.evansp.setup.user.def.rules;
+
+import gr.evansp.common.Rule;
+import gr.evansp.setup.user.def.models.User;
+
+/**
+ * Validator for {@link User}. User must follow the rules below,
+ * in order to be validated:
+ * <li>userId must not be null</li>
+ * <li>email must not be null</li>
+ * <li>email must contain the '@' character</li>
+ * <li>email must contain the '.' character, after the '@' character</li>
+ * <li>password must not be null.</li>
+ * <li>password must be at least 10 characters.</li>
+ * <li>password must contain at least 1 number, 1 character and 1 symbol.</li>
+ */
+public interface UserValidator extends Rule<User> {
+  //EMPTY
+}
